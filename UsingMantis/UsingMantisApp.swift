4b09837af9e242476da2c231f7f088509cd3a8b0
Jeongmin <tdcian71@gmail.com>
2021-05-23 16:23:02 +0900

    Initial Commit

---
+++ UsingMantis/UsingMantisApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingMantisApp.swift
+//  UsingMantis
+//
+//  Created by APPLE on 2021/05/23.
+//
+
+import SwiftUI
+
+@main
+struct UsingMantisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
